81449adb772071e8868e662434cd78222ef5698f
James Brown <jamesbrown5292@gmail.com>
2020-09-20 22:47:10 +0000

    add query skeleton syntax

--- Queries-helpers/board-queries.js
+++ Queries-helpers/board-queries.js
@@ -3,25 +3,14 @@
 
 const { db } = require('../server.js');
 
-// Define helper functions for retrieving, editing, deleting boards
-const getAllBoards = function() {
-  return db.query(`
-  SELECT name, boards.title, boards.description
-  FROM boards
-  JOIN users ON users.id = boards.owner_id
-  LIMIT 4;`)
-    .then((response) => {
-      return response.rows;
-    });
-};
-
 /* ### QUERY IDEAS ###
 
 ## BROWSE/READ OPERATIONS
--- Get all boards
+-- Get all boards [X]
 -- Get board by owner name
 -- Get board by owner id
--- Get board by category
+-- Get board by board id
+-- Get board by categories
 -- Get board by title
 -- Get all boards (order by highest/lowest rated)
 -- Get all boards (order by most commented)
@@ -33,7 +22,7 @@ const getAllBoards = function() {
 ## EDIT OPERATIONS
 -- Change board title
 -- Change board description
--- Add resource to board (can also be done via from a resource that already exists in other board, in this case clone it then insert as normal)
+-- (best handled in resources queries) Add resource to board (can also be done via from a resource that already exists in other board, in this case clone it then insert as normal)
 -- Add category to board
 
 ## DELETE OPERATIONS
@@ -42,6 +31,109 @@ const getAllBoards = function() {
 *** NOTE: Inserting a resource into my board from somebody else's board will require generating a new instance of the resource.
           This is probably not a very efficient use of space in the db but we want to give owners full autonomy and ownership of every resource in every board they own.
 */
+// Define helper functions for retrieving, editing, deleting boards
+const getAllBoards = function() {
+
+  //We should also handle the ORDER BY filters in this function call
+  return db.query(`
+  SELECT name, boards.title, boards.description
+  FROM boards
+  JOIN users ON users.id = boards.owner_id
+  LIMIT 4;`)
+    .then((response) => {
+      return response.rows;
+    });
+};
+
+const getBoardByOwnerName = function() {
+  return db.query(`
+  `)
+    .then((response) => {
+      return response.rows;
+    });
+};
+
+const getBoardByOwnerId = function() {
+  return db.query(`
+  `)
+    .then((response) => {
+      return response.rows;
+    });
+};
+
+const getBoardById = function() {
+  return db.query(`
+  `)
+    .then((response) => {
+      return response.rows;
+    });
+};
+
+const getBoardByCategories = function() {
+  return db.query(`
+  `)
+    .then((response) => {
+      return response.rows;
+    });
+};
+
+const getBoardByTitle = function() {
+  return db.query(`
+  `)
+    .then((response) => {
+      return response.rows;
+    });
+};
+
+const addNewBoard = function() {
+  return db.query(`
+  `)
+    .then((response) => {
+      return response.rows;
+    });
+};
+
+const editBoardTitle = function() {
+  return db.query(`
+  `)
+    .then((response) => {
+      return response.rows;
+    });
+};
+
+const editBoardTitle = function() {
+  return db.query(`
+  `)
+    .then((response) => {
+      return response.rows;
+    });
+};
+
+const editBoardCategories = function() {
+  return db.query(`
+  `)
+    .then((response) => {
+      return response.rows;
+    });
+};
+
+const editBoardDescription = function() {
+  return db.query(`
+  `)
+    .then((response) => {
+      return response.rows;
+    });
+
+const deleteBoard = function() {
+  return db.query(`
+  `)
+    .then((response) => {
+      return response.rows;
+    });
+
+
+};
+
 
 
 module.exports = {
