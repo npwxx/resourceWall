ab794654d196aa79374960b4ee8a4c0684ca9bfa
Nicole Woodcock <nicole.xox@gmail.com>
2020-09-20 21:36:25 +0000

    change getAllBoards to return all rows

--- Queries-helpers/board-queries.js
+++ Queries-helpers/board-queries.js
@@ -1,18 +1,18 @@
 // PG database client/connection setup
 //Is this the correct way to connect ^^ can we ust require db like andy did?
 
-const {db} = require('../server.js');
+const { db } = require('../server.js');
 
 // Define helper functions for retrieving, editing, deleting boards
 const getAllBoards = function() {
   return db.query(`SELECT * FROM BOARDS LIMIT 4;`)
-  .then((response) => {
-    return response.rows[0];
-  })
+    .then((response) => {
+      return response.rows;
+    });
 };
 
 
 module.exports = {
   //throw in here every function name you want to export
   getAllBoards
-}
+};
