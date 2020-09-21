6c4d1bf8893bf0c4d9bec5336b29f6450f2729cc
James Brown <jamesbrown5292@gmail.com>
2020-09-21 18:06:03 +0000

    add ancestor id

--- db/schema/05_resources.sql
+++ db/schema/05_resources.sql
@@ -8,5 +8,6 @@ CREATE TABLE resources (
   title VARCHAR(255) NOT NULL,
   resource_url VARCHAR (255) NOT NULL,
   description TEXT,
-  date_posted DATETIME NOT NULL
+  date_posted DATETIME NOT NULL,
+  ancestor_id INT
 );
