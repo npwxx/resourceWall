149a11a09a1bd86c1df1547975b4de4f6bff3ef4
Nicole Woodcock <nicole.xox@gmail.com>
2020-09-21 22:25:14 +0000

    ammend renderProfilePage function

--- public/scripts/authentication.js
+++ public/scripts/authentication.js
@@ -27,10 +27,16 @@ const renderProfilePage = function() {
   <form id="profile-form" >
   <div class="row gtr-uniform">
   <div class="col-6 col-12-xsmall">
-    <input type="text" name="demo-name" id="demo-name" value="" placeholder="Name" />
+    <input type="text" name="demo-name" id="demo-name" value="" placeholder="Update Name" />
   </div>
   <div class="col-6 col-12-xsmall">
-    <input type="email" name="demo-email" id="demo-email" value="" placeholder="Email" />
+    <input type="email" name="demo-email" id="demo-email" value="" placeholder="Update Email" />
+  </div>
+  <div class="col-6 col-12-xsmall">
+    <input type="password" name="password" id="password" value="" placeholder="Update Password" />
+  </div>
+  <div class="col-6 col-12-xsmall">
+    <input type="password" name="password" id="password" value="" placeholder="Current Password" />
   </div>
   </div>
   </form>
