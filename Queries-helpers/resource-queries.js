d699217a5afc371e78776158a116bfb861ef5a63
James Brown <jamesbrown5292@gmail.com>
2020-09-22 00:03:23 +0000

    add resource by rating query

--- Queries-helpers/resource-queries.js
+++ Queries-helpers/resource-queries.js
@@ -10,6 +10,23 @@ const getResourcesByBoardId = function(boardId) {
   });
 }
 
+const getResourcesByHighestRated = function () {
+  return db.query(`
+  SELECT
+    title,
+    left(description, 35) as description,
+    left(resource_url, 20) as link,
+    round(avg(resource_ratings.rating), 2) as average_rating
+  FROM resources
+  JOIN resource_ratings ON resources.id = resource_ratings.resource_id
+  GROUP BY resources.title, resources.description, resources.resource_url
+  ORDER BY average_rating DESC;
+`)
+    .then((response) => {
+    return response.rows;
+  });
+}
 module.exports =  {
-
+  getResourcesByBoardId,
+  getResourcesByHighestRated
 }
