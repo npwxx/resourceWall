c3af93d5023ee3c9eae49aa155d3ea2949d64584
Nicole Woodcock <nicole.xox@gmail.com>
2020-09-23 20:00:18 +0000

    add search framework for main page

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,17 +1,25 @@
 // MAIN PAGE FUNCTION
 const renderMainPageLayout = function() {
   $('#main').html(`<div class="inner">
-  <div>
+  <div class='board-search'>
   <header>
     <h1>Top Boards</h1>
+    <form class='board-category-search'>
+      <input type='text' placeholder='Search Categories'></input>
+      <button type='submit'><i class='fa fa-search'></i></button>
+    </form>
     <p>TODO: Add search bar & functionality FILTER BY users, category</p>
   </header>
   <section class="tiles">
   </section>
   </div>
-  <div>
+  <div class='resource-search'>
   <header>
     <h1>Top Resources</h1>
+    <form class='resource-category-search'>
+      <input type='text' palceholder='Search Categories' name='search'></input>
+      <button type='submit'><i class='fa fa-search'></i></button>
+    </form>
     <p>TODO: Add search bar & functionality FILTER BY category</p>
   </header>
   <section class="resources">
@@ -19,3 +27,71 @@ const renderMainPageLayout = function() {
   </div>
 </div>`);
 };
+
+// // SEACH FUNCTIONS
+// // boards
+// <form accept-charset="utf-8" action="/s/ref=nb_sb_noss" class="nav-searchbar" method="GET" name="board-search" role="search">
+//   <div class="nav-left">
+//     <div id="nav-search-dropdown-card">
+//       <div class="nav-search-scope nav-sprite">
+//         <div class="nav-search-facade" data-value="search-alias=aps">
+//           <span class="nav-search-label" style="width: auto;">All</span>
+//           <i class="nav-icon"></i>
+//         </div>
+//         <span id="searchDropdownDescription" style="display:none">Select the department you want to search in</span>
+//         <select aria-describedby="searchDropdownDescription" class="nav-search-dropdown searchSelect" data-nav-digest="x91FRHH5Ey0T3w80iSqEjQsbgCw=" data-nav-selected="0" id="searchDropdownBox" name="url" style="display: block; top: 2.5px;" tabindex="0" title="Search in">
+//           <option selected="selected" value="baords">categories</option>
+//           <option value="search-alias=alexa-skills">users</option>
+//         </select>
+//       </div>
+
+//     </div>
+//   </div>
+//   <div class="nav-fill">
+//     <div class="nav-search-field ">
+//       <input type="text" id="twotabsearchtextbox" value="" name="field-keywords" autocomplete="off" placeholder="" class="nav-input" dir="auto" tabindex="0" aria-label="Search">
+//       </div>
+//       <div id="nav-iss-attach"></div>
+//     </div>
+//     <div class="nav-right">
+//       <div class="nav-search-submit nav-sprite">
+//         <span id="nav-search-submit-text" class="nav-search-submit-text nav-sprite" aria-label="Go">
+//           <input type="submit" class="nav-input" value="Go" tabindex="0">
+//         </span>
+//       </div>
+//       </div>
+//   </form>
+//     {/* resources */}
+//     <form accept-charset="utf-8" action="/s/ref=nb_sb_noss" class="nav-searchbar" method="GET" name="site-search" role="search">
+//       <div class="nav-left">
+//         <div id="nav-search-dropdown-card">
+
+//           <div class="nav-search-scope nav-sprite">
+//             <div class="nav-search-facade" data-value="search-alias=aps">
+//               <span class="nav-search-label" style="width: auto;">All</span>
+//               <i class="nav-icon"></i>
+//             </div>
+//             <span id="searchDropdownDescription" style="display:none">Select the department you want to search in</span>
+//             <select aria-describedby="searchDropdownDescription" class="nav-search-dropdown searchSelect" data-nav-digest="x91FRHH5Ey0T3w80iSqEjQsbgCw=" data-nav-selected="0" id="searchDropdownBox" name="url" style="display: block; top: 2.5px;" tabindex="0" title="Search in">
+//               <option selected="selected" value="search-alias=aps">category</option>
+//               <option value="search-alias=alexa-skills">users</option>
+
+//             </select>
+//           </div>
+
+//         </div>
+//       </div>
+//       <div class="nav-fill">
+//         <div class="nav-search-field ">
+//           <input type="text" id="twotabsearchtextbox" value="" name="field-keywords" autocomplete="off" placeholder="" class="nav-input" dir="auto" tabindex="0" aria-label="Search">
+//       </div>
+//           <div id="nav-iss-attach"></div>
+//         </div>
+//         <div class="nav-right">
+//           <div class="nav-search-submit nav-sprite">
+//             <span id="nav-search-submit-text" class="nav-search-submit-text nav-sprite" aria-label="Go">
+//               <input type="submit" class="nav-input" value="Go" tabindex="0">
+//         </span>
+//       </div>
+//           </div>
+//   </form>
